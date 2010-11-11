d695e24b80377a010b42fba0b2c1abe63ced5000
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-11-10 23:47:00 -0600

    it works!

---
+++ lib/Log/Structured.pm
@@ -0,0 +1,100 @@
+package Log::Structured;
+
+use Moo;
+use Sub::Quote;
+
+use Time::HiRes qw(gettimeofday tv_interval);
+
+has log_event_listeners => (
+  is => 'ro',
+  isa => quote_sub(q<
+    die "log_event_listeners must be an arrayref!"
+      unless ref $_[0] && ref $_[0] eq 'ARRAY';
+
+    for (@{$_[0]}) {
+      die "each log_event_listener must be an coderef!"
+        unless ref $_ && ref $_ eq 'CODE';
+    }
+  >),
+  default => quote_sub q{ [] },
+);
+
+has "log_$_" => (
+  is => 'rw',
+)for qw(
+  milliseconds_since_start milliseconds_since_last_log
+  line file package subroutine category priority
+  date host pid
+);
+
+sub add_log_event_listener {
+  my $self = shift;
+
+  die "each log_event_listener must be an coderef!"
+    unless ref $_[1] && ref $_[1] eq 'CODE';
+
+   push @{$self->log_event_listeners}, $_[1]
+}
+
+sub log_event {
+   my $self = shift;
+   my $event_data = shift;
+
+   $self->${\"log_$_"} and $event_data->{$_} ||= $self->$_ for qw(
+      milliseconds_since_start milliseconds_since_last_log
+      line file package subroutine category priority
+      date host pid
+   );
+
+   $self->$_($event_data) for @{$self->log_event_listeners}
+}
+
+sub milliseconds_since_start {
+   int tv_interval(shift->{start_time}, [ gettimeofday ]) * 1000
+}
+
+sub milliseconds_since_last_log {
+   int tv_interval(shift->{last_event}, [ gettimeofday ]) * 1000
+}
+
+sub line { shift->_caller->[2] }
+
+sub file { shift->_caller->[1] }
+
+sub package { shift->_caller->[0] }
+
+sub subroutine { shift->_caller->[3] }
+
+sub category { shift->{category} }
+
+sub message { shift->{message} }
+
+sub priority { shift->{priority} }
+
+sub _caller {
+  my $self = shift;
+  my $depth = $self->{caller_depth} || 0;
+  my $clan  = $self->{caller_clan};
+
+  $depth += 3;
+
+  if (defined $clan) {
+    my $c; do {
+      $c = caller ++$depth;
+    } while $c && $c =~ $clan;
+    return [caller $depth]
+  } else {
+    return [caller $depth]
+  }
+}
+
+sub date { return [localtime] }
+
+sub host {
+  require Sys::Hostname;
+  return Sys::Hostname::hostname()
+}
+
+sub pid { $$ }
+
+1;
--- t/basic.t
+++ t/basic.t
@@ -1,10 +1,28 @@
 use strictures 1;
 use Test::More;
+use JSON::XS;
 
-{
-   package Power::Logger;
+use Log::Structured;
 
-   use Moo;
+my $l_s = Log::Structured->new({
+  log_line       => 1,
+  log_file       => 1,
+  log_package    => 1,
+  log_subroutine => 1,
+  log_category   => 1,
+  log_priority   => 1,
+  date           => 1,
+  log_event_listeners => [sub {
+    print encode_json( $_[1] ) . "\n"
+  }],
+});
 
-}
+$l_s->log_event({
+   message => 'frew',
+});
+
+$l_s->log_event({
+   message => 'frew',
+   category => 'frew',
+});
 
