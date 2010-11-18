801175bce15a6d5a84cd7734de17f6425b2ddcc1
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-11-17 23:58:55 -0600

    first cut at docs

--- lib/Log/Structured.pm
+++ lib/Log/Structured.pm
@@ -126,3 +126,195 @@ sub _sound_depth {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+ use Log::Structured;
+
+ my $structured_log = Log::Structured->new({
+   category            => 'Web Server',
+   log_category        => 1,
+   priority            => 'trace',
+   log_priority        => 1,
+   log_file            => 1,
+   log_line            => 1,
+   log_date            => 1,
+   log_event_listeners => [sub {
+      my ($self, $e) = @_;
+      my @date = @{$e->{date}};
+
+      my $ymd_hms  = "$date[5]-$date[4]-$date[3] $date[2]:$date[1]:$date[0]";
+      my $location = "$e->{file}:$e->{line}";
+      warn "[$ymd_hms][$location][$e->{priority}][$e->{category}] $e->{message}"
+   }, sub {
+      open my $fh, '>>', 'log';
+      print {$fh} encode_json($_[1]) . "\n";
+   }],
+ });
+
+ $structured_log->log_event({ message => 'Starting web server' });
+
+ $structured_log->log_event({
+   message => 'Oh no!  The database melted!',
+   priority => 'fatal',
+   category => 'Core',
+ });
+
+=head1 DESCRIPTION
+
+This module is meant to produce logging data flexibly and powerfully.  All of
+the data that it produces can easilly be serialized or put into a database or
+whatever else you may want to do with it.
+
+=head1 ATTRIBUTES
+
+=head2 log_event_listeners
+
+C<ArrayRef[CodeRef]>, coderefs get called in order, as methods, with log events
+as an argument
+
+=head2 caller_clan
+
+A stringified regex representing packages to allow when getting any caller
+information (including stacktrace.)  Typically this will be used to B<exclude>
+things from the stack.  For example, to exclue L<DBIx::Class> and
+L<SQL::Abstract> from your call stack you'd do:
+
+ caller_clan => '^DBIx::Class|^SQL::Abstract',
+
+=head2 category
+
+String representing the category of the log event
+
+=head2 priority
+
+String representing the priority of the log event.  Should be debug, trace,
+info, warn, error, or fatal.
+
+=head2 start_time
+
+Returns an ArrayRef containing the time the object was instantiated
+
+=head2 last_event
+
+Returns an ArrayRef containing the last time a log event occurred
+
+=head2 caller_depth
+
+An integer caller levels to skip when getting any caller information (including
+stacktrace.)
+
+=head1 ATTRIBUTES TO ENABLE LOG DATA
+
+All of the following attributes will enable their respective data in the log
+event:
+
+=over 1
+
+=item * log_milliseconds_since_start
+
+=item * log_milliseconds_since_last_log
+
+=item * log_line
+
+=item * log_file
+
+=item * log_package
+
+=item * log_subroutine
+
+=item * log_category
+
+=item * log_priority
+
+=item * log_date
+
+=item * log_host
+
+=item * log_pid
+
+=item * log_stacktrace
+
+=back
+
+=head1 METHODS
+
+=head2 add_log_event_listener
+
+Takes a coderef to be added to the L</log_event_listeners>
+
+=head2 log_event
+
+Takes a hashref of the data to be passed to the event listeners.  All of the
+data except for C<message>, C<category>, and C<priority> will be automatically
+populated by the methods below, unless they are passed in.
+
+=head2 milliseconds_since_start
+
+Returns milliseconds since object has been instantiated
+
+=head2 milliseconds_since_last_log
+
+Returns milliseconds since previous log event
+
+=head2 line
+
+Returns the line at the correct depth
+
+=head2 file
+
+Returns the file at the correct depth
+
+=head2 package
+
+Returns the package at the correct depth
+
+=head2 subroutine
+
+Returns the subroutine at the correct depth
+
+=head2 date
+
+Returns an arrayref containing the results from C<localtime>
+
+=head2 host
+
+Returns the host of the machine being logged on
+
+=head2 pid
+
+Returns the pid of the process being logged
+
+=head2 stacktrace
+
+Returns the a stacktrace ending at the correct depth.  The stacktrace is an
+arrayref of arrayrefs, where the inner arrayrefs match the return values of
+caller in list context
+
+=head1 SEE ALSO
+
+Before initial release, all of the code from this module was actually part of
+L<Log::Sprintf>.  Unsurprisingly, this module continues to work extremely well
+with C<Log::Sprintf>.  For example, the L</SYNOPSIS>' example of instantiation
+could be rewritten as:
+
+ use Log::Structured;
+ use Log::Sprintf;
+
+ my $formatter = Log::Sprintf->new({ format => "[%d][%F:%L][%p][%c] %m" });
+
+ my $structured_log = Log::Structured->new({
+   category            => 'Web Server',
+   log_category        => 1,
+   priority            => 'trace',
+   log_priority        => 1,
+   log_file            => 1,
+   log_line            => 1,
+   log_date            => 1,
+   log_event_listeners => [sub {
+      warn $formatter->sprintf($_[1])
+   }, sub {
+      open my $fh, '>>', 'log';
+      print {$fh} encode_json($_[1]) . "\n";
+   }],
+ });
